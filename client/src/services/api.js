b013293f1b4064feaa8e0e323bfe88dd78d47b55
Shivateja2004 <gajjishivateja@gmail.com>
2026-05-25 19:25:39 +0530

    fix vercel vite permission issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,28 +1,46 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const getHeaders = () => {
 const token = Cookies.get('token');
-return token ? { Authorization: `Bearer ${token}` } : {};
+
+return token
+    ? { Authorization: `Bearer ${token}` }
+    : {};
 };
 
 const api = {
 get: async (url) => {
-    const response = await axios.get(`${BASE_URL}${url}`, { headers: getHeaders() });
+    const response = await axios.get(`${BASE_URL}${url}`, {
+    headers: getHeaders(),
+    });
     return response;
 },
+
 post: async (url, data) => {
-    const response = await axios.post(`${BASE_URL}${url}`, data, { headers: getHeaders() });
+    const response = await axios.post(
+    `${BASE_URL}${url}`,
+    data,
+    { headers: getHeaders() }
+    );
     return response;
 },
+
 put: async (url, data) => {
-    const response = await axios.put(`${BASE_URL}${url}`, data, { headers: getHeaders() });
+    const response = await axios.put(
+    `${BASE_URL}${url}`,
+    data,
+    { headers: getHeaders() }
+    );
     return response;
 },
+
 delete: async (url) => {
-    const response = await axios.delete(`${BASE_URL}${url}`, { headers: getHeaders() });
+    const response = await axios.delete(`${BASE_URL}${url}`, {
+    headers: getHeaders(),
+    });
     return response;
 },
 };
